c4549750b28a8f01beb3dc3ea1c25f7e546362c8
leeminseo0923 <leeminseo0923@gmail.com>
2020-12-05 13:44:50 +0900

    model

--- network.py
+++ network.py
@@ -48,10 +48,10 @@ class Network:
         model.add(tf.keras.layers.MaxPooling2D((2,2)))
         model.add(tf.keras.layers.Conv2D(64,(3,3),activation='relu'))
         model.add(tf.keras.layers.MaxPooling2D((2,2)))
+        model.add(tf.keras.layers.Dropout(0.5))
         model.add(tf.keras.layers.Flatten())
-        model.add(tf.keras.layers.Dense(64, activation='relu'))
         model.add(tf.keras.layers.Dense(8, activation='softmax'))
-        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['mae'])
         return model
 
     def fit(self, model, train_X, train_Y, test_X, test_Y):
